d485ab8215681c923fb78737d9ef81a0c3042e6f
Adam Rodriguez <adam_rodriguez@bitbucket.org>
2018-09-18 14:07:44 -0700

    fix indentation

--- src/UrlGenerator.php
+++ src/UrlGenerator.php
@@ -22,164 +22,164 @@ class UrlGenerator implements UrlGeneratorContract, IEnableRoutes
     }
 
     /**
-	 * Get the full URL for the current request.
-	 *
-	 * @return string
-	 */
-	public function full()
-	{
-		return $this->originalUrlGenerator->full();
-	}
-
-	/**
-	 * Get the current URL for the request.
-	 *
-	 * @return string
-	 */
-	public function current()
-	{
-		return $this->originalUrlGenerator->current();
-	}
-
-	/**
-	 * Get the URL for the previous request.
-	 *
-	 * @return string
-	 */
-	public function previous($fallback = false)
-	{
-		return $this->originalUrlGenerator->previous($fallback);
-	}
-
-	/**
-	 * Generate a absolute URL to the given path.
-	 *
-	 * @param  string  $path
-	 * @param  mixed  $extra
-	 * @param  bool|null  $secure
-	 * @return string
-	 */
-	public function to($path, $extra = array(), $secure = null)
-	{
-		return $this->originalUrlGenerator->to($path, $extra, $secure);
-	}
-
-	/**
-	 * Generate a secure, absolute URL to the given path.
-	 *
-	 * @param  string  $path
-	 * @param  array   $parameters
-	 * @return string
-	 */
-	public function secure($path, $parameters = array())
-	{
-		return $this->originalUrlGenerator->secure($path, $parameters);
-	}
-
-	/**
-	 * Generate a URL to an application asset.
-	 *
-	 * @param  string  $path
-	 * @param  bool|null  $secure
-	 * @return string
-	 */
-	public function asset($path, $secure = null)
-	{
-		return $this->originalUrlGenerator->asset($path, $secure);
+     * Get the full URL for the current request.
+     *
+     * @return string
+     */
+    public function full()
+    {
+        return $this->originalUrlGenerator->full();
     }
 
     /**
-	 * Generate a URL to a secure asset.
-	 *
-	 * @param  string  $path
-	 * @return string
-	 */
-	public function secureAsset($path)
-	{
-		return $this->originalUrlGenerator->secureAsset($path);
+     * Get the current URL for the request.
+     *
+     * @return string
+     */
+    public function current()
+    {
+        return $this->originalUrlGenerator->current();
     }
 
     /**
-	 * Force the schema for URLs.
-	 *
-	 * @param  string  $schema
-	 * @return void
-	 */
-	public function forceSchema($schema)
-	{
-		$this->originalUrlGenerator->forceSchema($schema);
-	}
-
-	/**
-	 * Get the URL to a named route.
-	 *
-	 * @param  string  $name
-	 * @param  mixed   $parameters
-	 * @param  bool  $absolute
-	 * @param  \Illuminate\Routing\Route  $route
-	 * @return string
-	 *
-	 * @throws \InvalidArgumentException
-	 */
-	public function route($name, $parameters = array(), $absolute = true, $route = null)
-	{
-		return $this->originalUrlGenerator->route($name, $parameters, $absolute, $route);
+     * Get the URL for the previous request.
+     *
+     * @return string
+     */
+    public function previous($fallback = false)
+    {
+        return $this->originalUrlGenerator->previous($fallback);
     }
 
     /**
-	 * Get the URL to a controller action.
-	 *
-	 * @param  string  $action
-	 * @param  mixed   $parameters
-	 * @param  bool    $absolute
-	 * @return string
-	 */
-	public function action($action, $parameters = array(), $absolute = true)
-	{
-		return $this->originalUrlGenerator->action($action, $parameters, $absolute);
+     * Generate a absolute URL to the given path.
+     *
+     * @param  string  $path
+     * @param  mixed  $extra
+     * @param  bool|null  $secure
+     * @return string
+     */
+    public function to($path, $extra = array(), $secure = null)
+    {
+        return $this->originalUrlGenerator->to($path, $extra, $secure);
     }
 
     /**
-	 * Set the forced root URL.
-	 *
-	 * @param  string  $root
-	 * @return void
-	 */
-	public function forceRootUrl($root)
-	{
-		$this->originalUrlGenerator->forceRootUrl($root);
-	}
-
-	/**
-	 * Determine if the given path is a valid URL.
-	 *
-	 * @param  string  $path
-	 * @return bool
-	 */
-	public function isValidUrl($path)
-	{
-		return $this->originalUrlGenerator->isValidUrl($path);
+     * Generate a secure, absolute URL to the given path.
+     *
+     * @param  string  $path
+     * @param  array   $parameters
+     * @return string
+     */
+    public function secure($path, $parameters = array())
+    {
+        return $this->originalUrlGenerator->secure($path, $parameters);
     }
 
     /**
-	 * Get the request instance.
-	 *
-	 * @return \Symfony\Component\HttpFoundation\Request
-	 */
-	public function getRequest()
-	{
-		return $this->originalUrlGenerator->getRequest();
-	}
+     * Generate a URL to an application asset.
+     *
+     * @param  string  $path
+     * @param  bool|null  $secure
+     * @return string
+     */
+    public function asset($path, $secure = null)
+    {
+        return $this->originalUrlGenerator->asset($path, $secure);
+    }
 
-	/**
-	 * Set the current request instance.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return void
-	 */
-	public function setRequest(Request $request)
-	{
-		$this->originalUrlGenerator->setRequest($request);
+    /**
+     * Generate a URL to a secure asset.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    public function secureAsset($path)
+    {
+        return $this->originalUrlGenerator->secureAsset($path);
+    }
+
+    /**
+     * Force the schema for URLs.
+     *
+     * @param  string  $schema
+     * @return void
+     */
+    public function forceSchema($schema)
+    {
+        $this->originalUrlGenerator->forceSchema($schema);
+    }
+
+    /**
+     * Get the URL to a named route.
+     *
+     * @param  string  $name
+     * @param  mixed   $parameters
+     * @param  bool  $absolute
+     * @param  \Illuminate\Routing\Route  $route
+     * @return string
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function route($name, $parameters = array(), $absolute = true, $route = null)
+    {
+        return $this->originalUrlGenerator->route($name, $parameters, $absolute, $route);
+    }
+
+    /**
+     * Get the URL to a controller action.
+     *
+     * @param  string  $action
+     * @param  mixed   $parameters
+     * @param  bool    $absolute
+     * @return string
+     */
+    public function action($action, $parameters = array(), $absolute = true)
+    {
+        return $this->originalUrlGenerator->action($action, $parameters, $absolute);
+    }
+
+    /**
+     * Set the forced root URL.
+     *
+     * @param  string  $root
+     * @return void
+     */
+    public function forceRootUrl($root)
+    {
+        $this->originalUrlGenerator->forceRootUrl($root);
+    }
+
+    /**
+     * Determine if the given path is a valid URL.
+     *
+     * @param  string  $path
+     * @return bool
+     */
+    public function isValidUrl($path)
+    {
+        return $this->originalUrlGenerator->isValidUrl($path);
+    }
+
+    /**
+     * Get the request instance.
+     *
+     * @return \Symfony\Component\HttpFoundation\Request
+     */
+    public function getRequest()
+    {
+        return $this->originalUrlGenerator->getRequest();
+    }
+
+    /**
+     * Set the current request instance.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     */
+    public function setRequest(Request $request)
+    {
+        $this->originalUrlGenerator->setRequest($request);
     }
 
     public function setRoutes(RouteCollection $routes)
