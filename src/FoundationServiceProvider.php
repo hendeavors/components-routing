ca7e8726bb142e0e624f1a3af9af723e565db8a2
Adam Rodriguez <adam_rodriguez@bitbucket.org>
2018-10-29 15:13:26 -0700

    remove unecessary code

--- src/FoundationServiceProvider.php
+++ src/FoundationServiceProvider.php
@@ -4,9 +4,7 @@ namespace Endeavors\Components\Routing;
 
 use Illuminate\Routing\Redirector;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Foundation\Http\FormRequest;
-use Symfony\Component\HttpFoundation\Request;
-use Illuminate\Contracts\Validation\ValidatesWhenResolved;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 
 class FoundationServiceProvider extends ServiceProvider
@@ -18,64 +16,7 @@ class FoundationServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
-        $this->registerRequestValidation();
         $this->registerRequestSignatureValidation();
-    }
-
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->configureFormRequests();
-    }
-
-    /**
-     * Configure the form request related services.
-     *
-     * @return void
-     */
-    protected function configureFormRequests()
-    {
-        $this->app->afterResolving(function(ValidatesWhenResolved $resolved) {
-            $resolved->validate();
-        });
-
-        $this->app->resolving(function(FormRequest $request, $app) {
-            $this->initializeRequest($request, $app['request']);
-
-            $request->setContainer($app)->setRedirector($app->make(Redirector::class));
-        });
-    }
-
-    /**
-     * Initialize the form request with data from the given request.
-     *
-     * @param  \Illuminate\Foundation\Http\FormRequest  $form
-     * @param  \Symfony\Component\HttpFoundation\Request  $current
-     * @return void
-     */
-    protected function initializeRequest(FormRequest $form, Request $current)
-    {
-        $files = $current->files->all();
-
-        $files = is_array($files) ? array_filter($files) : $files;
-
-        $form->initialize(
-            $current->query->all(), $current->request->all(), $current->attributes->all(),
-            $current->cookies->all(), $files, $current->server->all(), $current->getContent()
-        );
-
-        if ($session = $current->getSession()) {
-            $form->setSession($session);
-        }
-
-        $form->setUserResolver($current->getUserResolver());
-
-        $form->setRouteResolver($current->getRouteResolver());
     }
 
     /**
@@ -85,32 +26,20 @@ class FoundationServiceProvider extends ServiceProvider
      */
     public function registerRequestSignatureValidation()
     {
-        FormRequest::macro('hasValidSignature', function() {
+        Request::macro('hasValidSignature', function() {
             return URL::hasValidSignature($this ?? URL::getRequest());
         });
 
-        FormRequest::macro('hasInvalidSignature', function() {
+        Request::macro('hasInvalidSignature', function() {
             return !URL::hasValidSignature($this ?? URL::getRequest());
         });
 
-        FormRequest::macro('hasValidParameterSignature', function(array $parameters = []) {
+        Request::macro('hasValidParameterSignature', function(array $parameters = []) {
             return URL::hasValidParameterSignature($this ?? URL::getRequest(), $parameters);
         });
 
-        FormRequest::macro('hasInvalidParameterSignature', function(array $parameters = []) {
+        Request::macro('hasInvalidParameterSignature', function(array $parameters = []) {
             return !URL::hasValidParameterSignature($this ?? URL::getRequest(), $parameters);
-        });
-    }
-
-    /**
-     * Register the "validate" macro on the request.
-     *
-     * @return void
-     */
-    public function registerRequestValidation()
-    {
-        FormRequest::macro('validate', function(array $rules, ...$params) {
-            return validator()->validate($this->all(), $rules, ...$params);
         });
     }
 }
